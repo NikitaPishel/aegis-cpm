6f9fc835f6a80262b0a6a1d848d8cace889be074
NikitaPishel <pogerproger@gmail.com>
2025-10-26 15:29:19 +0000

    fix: correct steel S355 yield strength

--- src/materials/baseMaterials.cpp
+++ src/materials/baseMaterials.cpp
@@ -5,7 +5,7 @@ using namespace cpm;
 namespace {
     int static registerMaterials() {
         Material steelS235(7850, 235e6);
-        Material steelS355(7850, 235e6);
+        Material steelS355(7850, 355e6);
         Material alluminum6061(2720, 276e6);
         Material alluminum2014(2800, 414e6);
         Material woodOak(770, 20e6);
